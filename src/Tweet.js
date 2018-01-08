017ce30358ae8ff6bd9688849997dc087c1d86b5
Edwin Rivera <waehake@gmail.com>
2018-01-08 11:50:44 -0500

    moved the skywalker tweet into its own file, imported it, passed it as a prop to the tweet component so that the data can be validated using prop-types

--- src/Tweet.js
+++ src/Tweet.js
@@ -8,46 +8,48 @@ import RetweetButton from './RetweetButton';
 import LikeButton from './LikeButton';
 import MoreOptionsButton from './MoreOptionsButton';
 import Comments from './Comments';
+import PropTypes from 'prop-types';
 
 class Tweet extends Component {
   render() {
-    var skywalkerTweet = {
-      message: 'Echo Three to Echo Seve, @Han, old buddy, do you read me?',
-      author: {
-        name: 'Luke Skylwalker',
-        handle: '@lukeskywalker',
-      },
-      likes: 3,
-      retweets: 42,
-      timestamp: '2017-11-20 21:24:37',
-    };
-
+    const { author, timestamp, message, retweets, likes } = this.props.skywalkerTweet;
     return (
       <div>
         <div className="tweet">
           <Avatar />
           <div className="content">
-            <NameWithHandle name={skywalkerTweet.author.name} handle={skywalkerTweet.author.handle}/>
-            <Time currentTime={skywalkerTweet.timestamp} />
-            <Message message={skywalkerTweet.message}/>
+            <NameWithHandle name={author.name} handle={author.handle}/>
+            <Time currentTime={timestamp} />
+            <Message message={message}/>
             <div className="buttons">
               <ReplyButton />
-              <RetweetButton retweets={skywalkerTweet.retweets} />
-              <LikeButton likes={skywalkerTweet.likes} />
+              <RetweetButton retweets={retweets} />
+              <LikeButton likes={likes} />
               <MoreOptionsButton />
             </div>
           </div>
         </div>
         <div>
           <Comments
-            handle={skywalkerTweet.author.handle}
-            comment={skywalkerTweet.message}
-            likes={skywalkerTweet.likes}
+            handle={author.handle}
+            comment={message}
+            likes={likes}
             />
         </div>
       </div>
     );
   }
 }
+
+Tweet.propTypes = {
+  skywalkerTweet: PropTypes.shape({
+    author: PropTypes.shape({
+      handle: PropTypes.string,
+      name: PropTypes.string,
+    }),
+    message: PropTypes.string,
+    likes: PropTypes.number,
+  }).isRequired,
+};
 
 export default Tweet;
---
+++ src/data.js
@@ -0,0 +1,12 @@
+var skywalkerTweet = {
+  message: 'Echo Three to Echo Seve, @Han, old buddy, do you read me?',
+  author: {
+    name: 'Luke Skylwalker',
+    handle: '@lukeskywalker',
+  },
+  likes: 3,
+  retweets: 42,
+  timestamp: '2017-11-20 21:24:37',
+};
+
+export default skywalkerTweet;
